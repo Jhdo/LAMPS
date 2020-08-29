09fe8df3d9c40e687bc234f0d328de105c87a208
hj0521 <hj0521.lee@gmail.com>
2020-08-29 16:00:19 +0900

    hjrevi

--- notice/src/nkv792/NKV792.cc
+++ notice/src/nkv792/NKV792.cc
@@ -190,7 +190,7 @@ unsigned long NKV792::ADCRead_Status1(int devnum, unsigned long mid)
 
   v = v & 0xF;
 
-  cout << "Status 1 : " << bitset<32>(v) << endl;
+//  cout << "Status 1 : " << bitset<32>(v) << endl;
 
   return v;
 }
