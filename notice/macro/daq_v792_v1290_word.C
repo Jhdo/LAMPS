141aa10233cb069bab20ed2466c5946cfa8be8c3
jhdo <jaehyeon.do@gmail.com>
2020-10-23 18:40:48 +0900

    U

--- notice/macro/daq_v792_v1290_word.C
+++ notice/macro/daq_v792_v1290_word.C
@@ -91,6 +91,8 @@ void daq_v792_v1290_word(int nevt = 3000)
     unsigned long nw_tdc = tdc_module->TDCRead_Buffer_MEB(devnum, moduleID_tdc, words_tdc);
     unsigned long nw_adc = adc_module->ADCRead_Buffer(devnum, moduleID_adc, words_adc);
 
+    if (icycle == 0) continue;
+
     auto elapsed = std::chrono::high_resolution_clock::now() - start;
     long microseconds = std::chrono::duration_cast<std::chrono::microseconds>(elapsed).count();
     std::cout << "Elapsed time 1 : " << microseconds << " micro seconds" << std::endl;
